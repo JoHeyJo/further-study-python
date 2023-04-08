1515cc67cff74d5f6575856013670580a7a8e433
jf <jpf0628@gmail.com>
2023-04-07 18:09:20 -0700

    wip: PostFrom - refactor interface for IPost, break up into appropriate interface and then extend to integrate with post data

--- sqlAlchemy/blogly/frontend/src/PostForm.tsx
+++ sqlAlchemy/blogly/frontend/src/PostForm.tsx
@@ -1,36 +1,64 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import { userGet } from './api';
-import { IUser } from "./interface";
+import { IUser, IPost } from "./interface";
+import { Button } from "react-bootstrap";
 
 /** Handles/ submits post data & renders form for new post 
  * 
  * State:
+ * - user: {id:0,firstName:'',lastName:'', image:''}
  * - post = {title:'', content:''}
  * 
  * Post -> PostForm
 */
-function PostForm({}){
-  const [user, setUser] = useState<IUser>({id:0,firstName:'',lastName:'', image:''})
+function PostForm({ }) {
+  const [userData, setUserData] = useState<IUser>({ id: 0, firstName: '', lastName: '', image: '' })
+  const [postData, setPostData] = useState<IPost>({title:'',content:''})
 
   const params = useParams();
   const userId = +params.id!
 
+  /** fetches user data on mount*/
   useEffect(() => {
-    async function fetchUser(){
+    async function fetchUser() {
       const res = await userGet(userId);
-      setUser(res);
+      setUserData(res);
     };
     fetchUser();
-  },[])
+  }, [])
 
+  /** handles changes in form */
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>){
+    e.preventDefault();
+    console.log(e.target.name)
+    const { name, value } = e.target
+    setUserData(u => ({
+      ...u,
+      [name]: value
+    }))
+  }
 
   return (
     <>
-    <h1>Add Post for {user.firstName} {user.lastName}</h1>
-    <form>
-      
-    </form>
+      <h1>Add Post for {userData.firstName} {userData.lastName}</h1>
+      <form  className="PostForm-form">
+
+        <label htmlFor="form-title">Title:</label>
+        <input className="PostForm-title" 
+        onChange={handleChange}
+         id="form-title"
+          name="title" />
+
+        <label htmlFor="form-content">Content:</label>
+        <input className="PostForm-content"
+         onChange={handleChange}
+          id="form-content"
+           name="content" />
+
+        <Button variant="info">Cancel</Button>
+        <Button variant="success">Submit</Button>
+      </form>
     </>
   )
 }
