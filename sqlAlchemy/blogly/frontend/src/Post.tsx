8f1afc984cd5e80e1415b7c4f07232f692fd8ecc
jf <jpf0628@gmail.com>
2023-04-29 11:44:48 -0700

    minimal styling to User

--- sqlAlchemy/blogly/frontend/src/Post.tsx
+++ sqlAlchemy/blogly/frontend/src/Post.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import { useParams, Link, useNavigate } from "react-router-dom";
+import Stack from 'react-bootstrap/Stack';
 //components / modules
 import { IPostData } from './interface';
 import { postGet, postDelete } from './api';
@@ -44,29 +45,40 @@ function Post() {
   }, [])
 
   /** Deletes user post */
-  async function deletePost(){
-    try{
+  async function deletePost() {
+    try {
       const res = await postDelete(postId)
       navigate(`/users/${post.userId}`)
-    } catch(error: any){
+    } catch (error: any) {
       console.error(`Error in deletePost => ${error}`)
     }
   }
 
   return (
-    <div>
-      <h1 className="Post-title">{post.title}</h1>
-      <h3 className="Post-content">{post.content}</h3>
-      <h3 className="Post-content">{post.problem}</h3>
-      <h3 className="Post-content">{post.content}</h3>
-      <h4 className="Post-author">By: {post.firstName} {post.lastName}</h4>
+    <>
+      <Stack gap={3}>
+        <h1 className="Post-title bg-light border">{post.title}</h1>
+        <Stack direction="horizontal" className="justify-content-center">
+          <h6 className="Post-subtitle">Context:</h6>
+          <h3 className="Post-content ms-2">{post.content}</h3>
+        </Stack>
+        <Stack direction="horizontal" className="justify-content-center">
+          <h6 className="Post-subtitle">Problem:</h6>
+          <p className="Post-problem ms-2">{post.problem}</p>
+        </Stack>
+        <Stack direction="horizontal" className="justify-content-center">
+          <h6 className="Post-subtitle">Solution:</h6>
+          <p className="Post-solution ms-2">{post.solution}</p>
+        </Stack>
+        <h6 className="Post-author">By: {post.firstName} {post.lastName}</h6>
+      </Stack>
       <div className="Post-controls">
         <Link to={`/users/${post.userId}`}><Button variant="outline-primary">Cancel</Button></Link>
         <Link to={`/posts/${postId}/edit`}><Button variant="primary">Edit</Button></Link>
         <Button onClick={deletePost} variant="danger">Delete</Button>
-        
+
       </div>
-    </div>
+    </>
   )
 }
 
