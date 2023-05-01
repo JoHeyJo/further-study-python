23a73d028024ea1c11d4cebc7ec784325e1d9926
jf <jpf0628@gmail.com>
2023-04-30 20:32:28 -0700

    added some styling to homepage

--- sqlAlchemy/blogly/frontend/src/HomePages.tsx
+++ sqlAlchemy/blogly/frontend/src/HomePages.tsx
@@ -1,8 +1,11 @@
+//Dependencies
 import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
 import { postsGetAll } from './api'
-import { IPosts, IPost } from "./interface";
+//Modules
+//Components
 import { Link } from 'react-router-dom';
-
+import { IPosts, IPost } from "./interface";
 
 /** Homepage for Bugly. Shows 5 most recent posts 
  * 
@@ -17,23 +20,24 @@ function HomePage() {
     async function fetchPosts() {
       let res = await postsGetAll();
       res.reverse()
-      res = res.slice(0,6)
+      res = res.slice(0, 6)
       setPosts(res)
     }
     fetchPosts()
-  },[])
+  }, [])
   return (
     <>
-    <h1>Blogly Recent Posts</h1>
-      {posts.map((post,i) => 
-      <section key={i}>
-        <Link to={`/posts/${post.id}`}>
-        <h2>{post.title}</h2></Link>
-        <h4>{post.content}</h4>
-        <h6>By {post.firstName} {post.lastName} {post.createdAt}</h6>
-       </section>
+      <h1 className="bg-light border">Blogly Recent Posts</h1>
+      {posts.map((post, i) =>
+        <section key={i}>
+          <Link to={`/posts/${post.id}`}>
+            <h2>{post.title}</h2>
+          </Link>
+          <h4>{post.content}</h4>
+          <h6>By {post.firstName} {post.lastName} {post.createdAt}</h6>
+        </section>
       )}
-      
+
     </>
   )
 }
--- sqlAlchemy/blogly/frontend/src/Users.tsx
+++ sqlAlchemy/blogly/frontend/src/Users.tsx
@@ -1,7 +1,7 @@
 //dependencies
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 //components
 import { IUser } from './interface'
 import { usersGet } from "./api";
@@ -25,21 +25,23 @@ function Users() {
       setUsers(res)
     }
     fetchUsers();
-  },[])
+  }, [])
 
   return (
-    <div className="Users-container">
-      <h1>
-        Users
-      </h1>
-      <div className="Users-box">
-        {users.map(u => <li key={u.id} className="Users-user"><Link to={`/users/${u.id}`} > {u.firstName} {u.lastName}</Link></li>)}
+    <Container>
+      <div className="Users-container">
+        <h1>
+          Users
+        </h1>
+        <div className="Users-box">
+          {users.map(u => <li key={u.id} className="Users-user"><Link to={`/users/${u.id}`} > {u.firstName} {u.lastName}</Link></li>)}
+        </div>
+        <Link to="/form">
+          <Button variant="primary">Add user</Button>
+        </Link>
+        <HomePage />
       </div>
-      <Link to="/form">
-        <Button variant="primary">Add user</Button>
-      </Link>
-      <HomePage />
-    </div>
+    </Container>
   )
 }
 
